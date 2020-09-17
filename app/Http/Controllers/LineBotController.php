be5faaca7df541e272d3af1522d265eec601e208
Yuri Suzuki <lil.yf.bb7v@gmail.com>
2020-09-17 17:14:38 +0900

    repeat機能実装

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 
 class LineBotController extends Controller
 {
@@ -13,8 +17,30 @@ class LineBotController extends Controller
   }
   public function repeat(Request $request)
   {
-    Log::debug($request->header());
-    Log::debug($request->input());
-    Log::debug($request->getContent());
+    // Log::debug($request->header());
+    // Log::debug($request->input());
+    // Log::debug($request->getContent());
+
+    $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+    $lineBot = new LINEBot($httpClient,['channelSecret'=> env('LINE_CHANNEL_SECRET')]);
+
+    $signature = $request->header('x-line-signature');
+    if(!$lineBot->validateSignature($request->getContent(),$signature)){
+     abort(400,'Invalid signature');
+    }
+
+    $events = $lineBot->parseEventRequest($request->getContent(),$signature);
+    Log::debug($events);
+
+    foreach ($events as $event) {
+      if (!$event instanceof TextMessage) {
+          Log::debug('Non text message has come');
+          continue;
+      }
+
+      $replyToken = $event->getReplyToken();
+      $replyText = $event->getText();
+      $lineBot->replyText($replyToken, $replyText);
+    }
   }
 }
